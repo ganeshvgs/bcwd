b189425b75904d50e762f7e55a7c8cd9c3d53674
ganeshvgs <gg1049697@gmail.com>
2025-07-18 14:33:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
 import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Login from "./pages/Login";              // Warden Login
+import StudentLogin from "./pages/StudentLogin"; // Student Login
+import Navbar from "./components/Navbar";         // Your updated Navbar
 
 export default function App() {
   useEffect(() => {
@@ -14,9 +15,11 @@ export default function App() {
 
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/student-login" element={<StudentLogin />} />
       </Routes>
     </Router>
   );
