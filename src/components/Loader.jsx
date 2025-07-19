54bb4b7f7f3b9b6fd2f8816920ec2668485bc568
ganeshvgs <gg1049697@gmail.com>
2025-07-19 12:21:25 +0530

    Add files via upload

---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="fixed inset-0 bg-cream flex items-center justify-center z-50">
+      <div className="w-12 h-12 border-4 border-maroon border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
